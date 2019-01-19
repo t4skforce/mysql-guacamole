f97b0b38c886075152f95cb1aee7c8ee0bf6185f
t4skforce <t4skforce1337@gmail.com>
2019-01-19 11:09:53 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in ${SOURCE}/upgrade-pre-*.sql; do
+for filename in ./upgrade/upgrade-pre-*.sql; do
 	version=$(echo $filename | awk -F"-" '{ print $3 }' | awk -F"." '{ printf("%02d%02d%02d\n",$1,$2,$3) }')
 	mv $filename /docker-entrypoint-initdb.d/999-upgrade-${version}.sql
 done
