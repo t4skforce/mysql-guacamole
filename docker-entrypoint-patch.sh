d71cab63847ccd48a749828a531e7f564b0c60a3
t4skforce <t4skforce1337@gmail.com>
2019-01-19 11:38:20 +0100

    Create docker-entrypoint-patch.sh

---
+++ docker-entrypoint-patch.sh
@@ -0,0 +1,11 @@
+for f in /docker-entrypoint-initdb.d/*; do
+  case "$f" in
+    *.sh)     echo "$0: running $f"; . "$f" ;;
+    *.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+    *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
+    *)        echo "$0: ignoring $f" ;;
+  esac
+  echo
+done
+
+exec "$@"
