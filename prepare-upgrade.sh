f0cd8347195182bf1b5f9bed7b4cf29cd324ee2c
t4skforce <t4skforce1337@gmail.com>
2019-01-19 19:53:31 +0100

    Update prepare-upgrade.sh

--- prepare-upgrade.sh
+++ prepare-upgrade.sh
@@ -8,6 +8,7 @@ for filename in ./schema/upgrade/upgrade-pre-*.sql; do
 done
 
 # generate init.sql so upgrades are not run after init
+mkdir -p /docker-entrypoint-initdb.d/
 echo "CREATE TABLE IF NOT EXISTS \`auto_updates\` (\`hash\` varchar(32) NOT NULL UNIQUE) ENGINE=InnoDB DEFAULT CHARSET=utf8;" > /docker-entrypoint-initdb.d/999-upgrade.sql
 for f in /docker-entrypoint-upgrade.d/*; do
   md5=$(md5sum "${f}" | awk '{ print $1 }')
