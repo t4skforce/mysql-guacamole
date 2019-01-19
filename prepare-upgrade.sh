5877bba4b8a1282420633905d2b0830e701b4cad
t4skforce <t4skforce1337@gmail.com>
2019-01-19 11:26:54 +0100

    Update prepare-upgrade.sh

--- prepare-upgrade.sh
+++ prepare-upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p /docker-entrypoint-upgradedb.d/
-for filename in ./upgrade/upgrade-pre-*.sql; do
+mkdir -p /docker-entrypoint-upgrade.d/
+for filename in ./schema/upgrade/upgrade-pre-*.sql; do
 	version=$(echo $filename | awk -F"-" '{ print $3 }' | awk -F"." '{ printf("%02d%02d%02d\n",$1,$2,$3) }')
-	mv $filename /upgrade/999-upgrade-${version}.sql
+	mv $filename /docker-entrypoint-upgrade.d/999-upgrade-${version}.sql
 done
