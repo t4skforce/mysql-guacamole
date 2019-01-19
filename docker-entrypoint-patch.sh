0b08b2cd43af2c30966de69d80a422a30b2d8dfe
t4skforce <t4skforce1337@gmail.com>
2019-01-19 12:01:08 +0100

    Update docker-entrypoint-patch.sh

--- docker-entrypoint-patch.sh
+++ docker-entrypoint-patch.sh
@@ -1,11 +1,51 @@
-for f in /docker-entrypoint-initdb.d/*; do
-  case "$f" in
-    *.sh)     echo "$0: running $f"; . "$f" ;;
-    *.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
-    *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
-    *)        echo "$0: ignoring $f" ;;
-  esac
+
+if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
+	# still need to check config, container may have started with --user
+	_check_config "$@"
+  
+  echo 'Upgrading database'
+  for f in /docker-entrypoint-upgrade.d/*; do
+    case "$f" in
+      *.sh)
+        if [ $(echo "SELECT count(1) FROM auto_updates WHERE scriptname='${f}';" | "${mysql[@]}") -eq 0 ]; then
+          echo "$0: running $f";
+          . "$f"
+          echo "INSERT INTO auto_updates (scriptname) VALUES ('${f}');" | "${mysql[@]}"
+        else
+          echo "$0: ignoring $f"
+        fi
+        echo 
+      ;;
+      *.sql)
+        if [ $(echo "SELECT count(1) FROM auto_updates WHERE scriptname='${f}';" | "${mysql[@]}") -eq 0 ]; then
+          echo "$0: running $f";
+          "${mysql[@]}" < "$f";
+          echo "INSERT INTO auto_updates (scriptname) VALUES ('${f}');" | "${mysql[@]}"
+        else
+          echo "$0: ignoring $f"
+        fi 
+        echo 
+      ;;
+      *.sql.gz)
+        if [ $(echo "SELECT count(1) FROM auto_updates WHERE scriptname='${f}';" | "${mysql[@]}") -eq 0 ]; then
+          echo "$0: running $f";
+          gunzip -c "$f" | "${mysql[@]}";
+          echo "INSERT INTO auto_updates (scriptname) VALUES ('${f}');" | "${mysql[@]}"
+        else
+          echo "$0: ignoring $f"
+        fi
+        echo 
+      ;;
+      *)        
+        echo "$0: ignoring $f" 
+      ;;
+    esac
+    echo
+  done
+  
   echo
-done
+  echo 'MySQL update process done. Ready for start up.'
+  echo
+fi
 
 exec "$@"
