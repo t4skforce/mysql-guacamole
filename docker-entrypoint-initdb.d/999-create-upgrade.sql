a17b20773a17a435c4223abc77a275f8abec5483
t4skforce <t4skforce1337@gmail.com>
2019-01-19 13:14:52 +0100

    Delete 999-create-upgrade.sql

--- docker-entrypoint-initdb.d/999-create-upgrade.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Setup autoupgrade script tracking
-*/
-CREATE TABLE IF NOT EXISTS `auto_updates` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `scriptname` varchar(255) NOT NULL UNIQUE,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
